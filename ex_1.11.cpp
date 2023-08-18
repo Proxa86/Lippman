3ccbaa55949c19a417ac99b94e63f3bfe807d53a
Proxa86 <Proxa86@ya.ru>
2023-08-18 14:07:40 +0000

    Ex. 1.11 chapter 1.4.1 ex_1.11.cpp page 31

---
+++ ex_1.11.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+
+	int val1 = 0, val2 = 0;
+	std::cout << "Enter namber 1:\n";
+	std::cin >> val1;
+	std::cout << "Enter namber 2:\n";
+	std::cin >> val2;
+	
+	if(val1 > val2)
+	{
+		while(val2 <= val1)
+		{
+			std::cout << val2 << " ";
+			++val2; 
+		}
+	}
+	else
+	   {
+		while (val1 <= val2)
+		{
+			std::cout << val1 << " ";
+			++val1;
+		}
+	   }
+	std::cout << std::endl;
+	return 0;
+}
