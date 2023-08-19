067e67ac15564a9e6b2fd6a72c8515bb2670a547
Proxa86 <Proxa86@ya.ru>
2023-08-19 13:38:58 +0000

    Ex. 1.5.2 Sales_item.h Sales_item.cpp page 45

--- Sales_item.cpp
+++ Sales_item.cpp
@@ -9,8 +9,24 @@ int main()
     std::cin >> book;
     std::cout << book << std::endl;
     */
+    /*
     Sales_item item1, item2;
     std::cin >> item1 >> item2;
     std::cout << item1 + item2 << std::endl;
-    return 0;
+    */
+    Sales_item item1, item2;
+    std::cin >> item1 >> item2;
+    // Version 1
+    //if(item1.same_isbn(item2))
+    // Version 2
+    if(item1.same_isbn() == item2.same_isbn())
+    {
+        std::cout << item1 + item2 << std::endl;
+        return 0;
+    }
+    else
+    {
+        std::cerr << "Data must refer to same ISBN" << std::endl;
+        return -1;
+    }
 }
--- Sales_item.h
+++ Sales_item.h
@@ -57,6 +57,8 @@ public:
     double avg_price() const;
     bool same_isbn(const Sales_item &rhs) const
         { return isbn == rhs.isbn; }
+    std::string same_isbn()
+        { return isbn; }
     // default constructor needed to initialize members of built-in type
     Sales_item(): units_sold(0), revenue(0.0) { }
 // private members as before
